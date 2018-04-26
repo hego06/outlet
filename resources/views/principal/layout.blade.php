cffd8ced876644f5b614fc30ffa301d07042fb01
hego06 <33333619+hego06@users.noreply.github.com>
2018-04-26 18:32:25 -0500

    changed layout
    se agrego el yield para el titulo, descripcion y contenido principal

--- resources/views/principal/layout.blade.php
+++ resources/views/principal/layout.blade.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Starter</title>
+  <title>OUTLET</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
@@ -283,8 +283,8 @@ desired effect
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Page Header
-        <small>Optional description</small>
+      @yield('title')
+        <small>@yield('description')</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -295,9 +295,7 @@ desired effect
     <!-- Main content -->
     <section class="content container-fluid">
 
-      <!--------------------------
-        | Your Page Content Here |
-        -------------------------->
+      @yield('content')
 
     </section>
     <!-- /.content -->
